8b85762c3d6563fe86cae6ad6cac98081e826d5c
Yash Tripathi <tripathiyash6969@gmail.com>
2024-06-20 21:15:24 +0530

    Create Binary Search.py

---
+++ Binaary Search/Binary Search.py
@@ -0,0 +1,16 @@
+def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        s,e=0,len(nums)-1
+        while s<=e:
+          m=(s+e)//2
+          if nums[m]==target:
+            return m
+          elif nums[m]<target:
+            s=m+1
+          else :
+            e=m-1
+        return -1
